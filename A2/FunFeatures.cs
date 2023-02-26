6a72788b796d9bf77954ade4dd1ecfa440b9a23a
roseygirlforra <gulcinkanat97@gmail.com>
2023-02-26 23:46:47 +0100

    Organizing

--- A2/FunFeatures.cs
+++ A2/FunFeatures.cs
@@ -5,7 +5,13 @@ private string lastName = "";
 
 private string name = "";
 
-
+/*
+Asks the user to input a sentence. 
+Assigns the input to a string var called userText. 
+Prints the string in capital letters. 
+Assigns the length of the string to int var called countChars. 
+Prints it to the console. 
+*/
 public void CalculateStrengthLength(){
     Console.WriteLine("\n---- STRENGTH LENGTH -----");
     Console.WriteLine("\nWrite a text with any number of characters and press Enter.\nYou can even copy text from a file and paste it here!\n");
@@ -16,6 +22,13 @@ public void CalculateStrengthLength(){
 
 }
 
+/*
+Introduction about me. 
+Calls the methods that takes input of users name and email. 
+Creates a new string array called newName that stores two elements. 
+Prints the second element first that being last name, and first element as second that being first name. 
+Calls the methods CalculateStrengthLength() and PredictMyDay()
+*/
 private void Introduce(){
 
     Console.WriteLine("My name is Fenix and I am a student of the Spring 2023 semester. ");
@@ -33,6 +46,12 @@ PredictMyDay();
     
 }
 
+/*
+Asks user for input
+Takes input as string, converts to int and stores in an int variable. 
+Switch(int) in order to determine what user has chosen and what value will be returned to the user.
+If not between 1-7 then default value which says "there is no such day". 
+*/
 public void PredictMyDay(){
     int chosenDay;
 
@@ -80,6 +99,9 @@ public void PredictMyDay(){
 
 }
 
+/*
+Asks for users email, stores input in a string var. 
+*/
 public void ReadEmail(){
 
     Console.WriteLine("Your email please: ");
@@ -88,6 +110,10 @@ public void ReadEmail(){
 
 }
 
+/*
+Asks for users first and last name, stores in diff string vars. 
+Stores firstName, space and lastName in its converted form into the string variable "name". 
+*/
 public void ReadName(){
     Console.WriteLine("Your first name please:");
     firstName = Console.ReadLine();
@@ -98,12 +124,42 @@ public void ReadName(){
     name = firstName + " " + lastName.ToUpper();
 }
 
-private bool RunAgain(){
 
-    return true;
+/*
+Asks user for input, if yes, returns true. If no , returns false. if something else, prints "invalid" and asks for input again.
+*/
+private bool RunAgain(){
+    Console.WriteLine("Continue with another round? (yes/no)\n");
+    String userChoice = Console.ReadLine();
+
+    if(userChoice == "yes"){
+        return true;
+    }
+    else if(userChoice == "no"){
+    return false;
+    }else {
+        Console.WriteLine("Invalid input. Please enter 'yes' or 'no'.");
+        return RunAgain();
+    }
 }
 
+/*
+The start method for this project
+Calls introduce method and RunIteration method. */
 public void Start(){
+    
     Introduce();
+    RunIteration();
+   
+}
+
+/*
+While RunAgain is true, calls CalculateStrengthLength() and PredictMyDay()
+*/
+public void RunIteration(){
+    while(RunAgain()){
+        CalculateStrengthLength();
+        PredictMyDay();
+    }
 }
 }
--- A2/MainProgram.cs
+++ A2/MainProgram.cs
@@ -1,8 +1,20 @@
 ﻿
 public partial class MainProgram{
+
+//Calls Start() method from FunFeatures class. 
+//Calls its own method "ContinueNextPart()"
 static void Main(String [] args){
 FunFeatures fun = new FunFeatures();
 fun.Start();
+
+ContinueToNextPart();
+}
+
+//Takes input from user, clears the console.
+private static void ContinueToNextPart(){
+    Console.WriteLine("\nPress Enter to continue to the next part!");
+    Console.ReadLine();
+    Console.Clear();
 }
 
 }
