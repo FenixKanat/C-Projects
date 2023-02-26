65a2d2adad849f69fb6ff2e98544558468cc2674
roseygirlforra <gulcinkanat97@gmail.com>
2023-02-26 22:30:37 +0100

    Funny Console App in C sharp

--- A2/FunFeatures.cs
+++ A2/FunFeatures.cs
@@ -7,6 +7,12 @@ private string name = "";
 
 
 public void CalculateStrengthLength(){
+    Console.WriteLine("\n---- STRENGTH LENGTH -----");
+    Console.WriteLine("\nWrite a text with any number of characters and press Enter.\nYou can even copy text from a file and paste it here!\n");
+    string userText = Console.ReadLine();
+    Console.WriteLine("\n"+userText.ToUpper());
+    int countChars = userText.Length;
+    Console.WriteLine("\nNumber of chars = " + countChars);
 
 }
 
@@ -21,10 +27,56 @@ ReadEmail();
     String [] newName = name.Split(" ");
     Console.WriteLine("\nNice to meet you " + newName[1] + "," + newName[0] );
     Console.WriteLine("Your email is: " + email);
+
+CalculateStrengthLength();
+PredictMyDay();
     
 }
 
 public void PredictMyDay(){
+    int chosenDay;
+
+    Console.WriteLine("***** Predict MY DAY *****n"); 
+    Console.WriteLine("Select a number betwee 1 and 7");
+    String dayNum = Console.ReadLine();
+    chosenDay = int.Parse(dayNum);
+
+    switch(chosenDay){
+        case 1: 
+        Console.WriteLine("\nKeep calm on Mondays! You can fall apart!");
+        break;
+
+        case 2: 
+        Console.WriteLine("\nTuesdays break your heart!");
+        break;
+
+        case 3:
+        Console.WriteLine("\nWednesdays dont matter! ");
+        break; 
+
+        case 4: 
+        Console.WriteLine("\nThursday is your lucky day, don't wait for Friday!");
+        break;
+
+        case 5:
+        Console.WriteLine("\nFriday, you are in love!");
+        break; 
+
+        case 6: 
+        Console.WriteLine("\nSaturday, do nothing and do plenty of it!");
+        break;
+
+        case 7: 
+        Console.WriteLine("\nAnd Sunday always comes too soon");
+        break;
+
+        default:
+        Console.WriteLine("\nNo day? is a good day but it doesn't exist!");
+        break;
+        
+    }
+
+    
 
 }
 
